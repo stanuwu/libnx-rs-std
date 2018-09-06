3813b9c154223809e9a5bf23e5567ab291ddfa8b
ischeinkman <scheinkman.ilan@gmail.com>
2018-09-05 20:43:06 -0700

    Some more misc merge fixes.

--- src/libstd/lib.rs
+++ src/libstd/lib.rs
@@ -278,7 +278,6 @@
 #![feature(on_unimplemented)]
 #![feature(optin_builtin_traits)]
 #![feature(panic_internals)]
-#![feature(panic_unwind)]
 #![feature(pin)]
 #![feature(prelude_import)]
 #![feature(ptr_internals)]
@@ -314,6 +313,8 @@
 #![feature(non_exhaustive)]
 
 #![default_lib_allocator]
+
+#![feature(extern_prelude)]
 
 // Always use alloc_system during stage0 since we don't know if the alloc_*
 // crate the stage0 compiler will pick by default is enabled (e.g.
@@ -518,6 +519,7 @@ pub mod rt;
 #[allow(missing_debug_implementations, missing_docs, dead_code)]
 #[unstable(feature = "stdsimd", issue = "48556")]
 #[cfg(all(not(stage0), not(test)))]
+#[cfg(not(target_os = "horizon"))] //TODO: not need to disable stdsimd for horizon
 mod stdsimd;
 
 // A "fake" module needed by the `stdsimd` module to compile, not actually
@@ -529,6 +531,7 @@ mod coresimd {
 
 #[stable(feature = "simd_arch", since = "1.27.0")]
 #[cfg(all(not(stage0), not(test)))]
+#[cfg(not(target_os = "horizon"))] //TODO: not need to disable stdsimd for horizon
 pub use stdsimd::arch;
 
 // Include a number of private modules that exist solely to provide
