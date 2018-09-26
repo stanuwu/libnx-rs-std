6e484712a0a24a8cb7bd57c01c47c4c5b661febe
Josh Stone <jistone@redhat.com>
2018-09-26 14:26:46 -0700

    Bump to 1.31.0 and bootstrap from 1.30 beta

--- src/libstd/alloc.rs
+++ src/libstd/alloc.rs
@@ -205,14 +205,12 @@ pub mod __default_lib_allocator {
     // ABI
 
     #[rustc_std_internal_symbol]
-    #[cfg_attr(stage0, no_mangle)]
     pub unsafe extern fn __rdl_alloc(size: usize, align: usize) -> *mut u8 {
         let layout = Layout::from_size_align_unchecked(size, align);
         System.alloc(layout)
     }
 
     #[rustc_std_internal_symbol]
-    #[cfg_attr(stage0, no_mangle)]
     pub unsafe extern fn __rdl_dealloc(ptr: *mut u8,
                                        size: usize,
                                        align: usize) {
@@ -220,7 +218,6 @@ pub mod __default_lib_allocator {
     }
 
     #[rustc_std_internal_symbol]
-    #[cfg_attr(stage0, no_mangle)]
     pub unsafe extern fn __rdl_realloc(ptr: *mut u8,
                                        old_size: usize,
                                        align: usize,
@@ -230,7 +227,6 @@ pub mod __default_lib_allocator {
     }
 
     #[rustc_std_internal_symbol]
-    #[cfg_attr(stage0, no_mangle)]
     pub unsafe extern fn __rdl_alloc_zeroed(size: usize, align: usize) -> *mut u8 {
         let layout = Layout::from_size_align_unchecked(size, align);
         System.alloc_zeroed(layout)
